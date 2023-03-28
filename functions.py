ce8aab61aa7cb9ac7a6969479767bc6678bb084d
tkm5 <005tbg@gmail.com>
2023-03-28 16:04:18 +0900

    Split and refactor the code

---
+++ functions.py
@@ -0,0 +1,72 @@
+import os
+
+import whisper
+import pandas as pd
+
+import settings
+
+LANG = settings.LANG
+
+AUDIO_DIR = settings.AUDIO_DIR
+AUDIO_FILE = settings.AUDIO_FILE
+AUDIO_PATH = os.path.join(AUDIO_DIR, AUDIO_FILE)
+
+
+def elapsed_time_str(seconds):
+    seconds = int(seconds + 0.5)
+    h = seconds // 3600
+    m = (seconds - h * 3600) // 60
+    s = seconds - h * 3600 - m * 60
+    return f"{h:02}:{m:02}:{s:02}"
+
+
+def audio_to_text(model):
+    # Convert audio files to text
+    model = whisper.load_model(str(model))
+
+    result_dict = model.transcribe(audio=AUDIO_PATH,
+                                   verbose=True,
+                                   language=LANG)
+
+    minutes_dict = _generate_minutes_dict(result_dict['segments'])
+
+    # minutes_dict['start_time'] = list(map(elapsed_time_str, minutes_dict['start_time']))
+    minutes_dict['start_time'] = [elapsed_time_str(t) for t in minutes_dict['start_time']]
+    # minutes_dict['end_time'] = list(map(elapsed_time_str, minutes_dict['end_time']))
+    minutes_dict['end_time'] = [elapsed_time_str(t) for t in minutes_dict['end_time']]
+
+    return minutes_dict
+
+
+def _generate_minutes_dict(result):
+    start_times_list = []
+    end_times_list = []
+    texts_list = []
+
+    for result in result:
+        start_time = result['start']
+        start_times_list.append(start_time)
+        end_time = result['end']
+        end_times_list.append(end_time)
+        text = result['text']
+        texts_list.append(text)
+
+    minutes_dict = {
+        'start_time': start_times_list,
+        'end_time': end_times_list,
+        'speach_text': texts_list
+    }
+
+    return minutes_dict
+
+
+def write_to_text(text_path, minutes_dict):
+    with open(text_path, "w") as f:
+        for i in range(len(minutes_dict)):
+            print(f'[{minutes_dict["start_time"][i]}] --> [{minutes_dict["end_time"][i]}] | {minutes_dict["speach_text"][i]}',
+                  file=f)
+
+
+def export_csv(output_path, minutes_dict):
+    df = pd.DataFrame(minutes_dict)
+    df.to_csv(output_path)
--- main.py
+++ main.py
@@ -1,69 +1,17 @@
-import whisper
-import pandas as pd
+import os
 
-import slack_notification
+import functions
+import settings
 
-LANG = "ja"
-AUDIO_FILE = "test.m4a"
-# Do not need file extension
-OUTPUT_FILE_NAME = AUDIO_FILE.split(".")[0]
+OUTPUT_FILE_NAME = settings.OUTPUT_FILE_NAME
 
+TEXT_DIR = settings.TEXT_DIR
+CSV_DIR = settings.CSV_DIR
 
-def elapsed_time_str(seconds):
-    seconds = int(seconds + 0.5)
-    h = seconds // 3600
-    m = (seconds - h * 3600) // 60
-    s = seconds - h * 3600 - m * 60
+TEXT_PATH = os.path.join(TEXT_DIR, OUTPUT_FILE_NAME+".txt")
+CSV_PATH = os.path.join(CSV_DIR, OUTPUT_FILE_NAME+".csv")
 
-    return f"{h:02}:{m:02}:{s:02}"
-
-
-def audio_to_text(model):
-    # Convert audio files to text
-    model = whisper.load_model(str(model))
-    result_dict = model.transcribe(audio=AUDIO_FILE,
-                                   verbose=True,
-                                   language=LANG)
-    return result_dict
-
-
-def generate_start_time_end_time_text(result):
-
-    start_times_list = []
-    end_times_list = []
-    texts_list = []
-
-    for result in result:
-        start_time = result['start']
-        start_times_list.append(start_time)
-        end_time = result['end']
-        end_times_list.append(end_time)
-        text = result['text']
-        texts_list.append(text)
-
-    return start_times_list, end_times_list, texts_list
-
-
-def write_to_text(file_name=OUTPUT_FILE_NAME):
-    with open(OUTPUT_FILE_NAME + ".txt", "w") as f:
-        for i in range(len(texts)):
-            print(f'[{start_times[i]}] --> [{end_times[i]}] | {texts[i]}', file=f)
-
-
-def export_csv(output_file_name=OUTPUT_FILE_NAME):
-    df = pd.DataFrame(data={'start_time': start_times,
-                            'end_time': end_times,
-                            'speach_text': texts}
-                      )
-    df.to_csv(OUTPUT_FILE_NAME + ".csv")
-
-
-results = audio_to_text(model="large")
-start_times, end_times, texts = generate_start_time_end_time_text(results['segments'])
-
-start_times = list(map(elapsed_time_str, start_times))
-end_times = list(map(elapsed_time_str, end_times))
-
-write_to_text()
-export_csv()
-slack_notification.slack_dm()
+if __name__ == '__main__':
+    minutes_dict = functions.audio_to_text(model='large')
+    functions.write_to_text(TEXT_PATH, minutes_dict)
+    functions.export_csv(CSV_PATH, minutes_dict)
